5b68fe9c215459dcc5cb93d8e14587c03e69d731
SatyamBhosale2004 <satyambhosale.work@gmail.com>
2026-09-16 22:02:45 +0530

    Feat: q4 done

--- java/4th/StringAssign.java
+++ java/4th/StringAssign.java
@@ -1,5 +1,5 @@
 public class StringAssign {
-    public static void main(String args[]){
+    public static void q1(){
         System.out.println("Enter String");
         String s = ConsoleInput.getString();
         int index = 0;
@@ -11,7 +11,41 @@ public class StringAssign {
                     System.out.println("The character at position " + index + " is " +s.charAt(iTemp));
             }
         }while(index >= 0 && index < s.length());
-        
+    }
+    
+    
+    public static void q2(){
+        System.out.println("Enter String1 ");
+        String s1 = ConsoleInput.getString();
+        System.out.println("Enter String2 ");
+        String s2 = ConsoleInput.getString();
+        if(s1.compareTo(s2) == 0)
+            System.out.println("Lexicographically true " );
+        else
+            System.out.println("Lexicographically false " );
+    }
+    
+
+    public static void q3(){
+        System.out.println("Enter String 1 ");
+        String s1 = ConsoleInput.getString();
+        boolean flag = true;
+        while(flag){
+            System.out.println("Enter String 2 to check if str 1 ends with it or not");
+            String s2 = ConsoleInput.getString();
+            if(s1.endsWith(s2))
+                System.out.println("Same ending");
+            else{
+                System.out.println("Diff ending");
+                flag = false;
+            }
+        }
+    }
+    public static void main(String args[]){
+        // q1();
+        // q2();
+        q3();
 
     }
 }
+
